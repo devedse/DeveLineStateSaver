34ce10731e40903120790950e59bb5108f8f990e
Devedse <davydavidse@gmail.com>
2018-12-29 17:39:48 +0100

    Moved to choco 7zip package

--- Scripts/GoRepack.cmd
+++ Scripts/GoRepack.cmd
@@ -2,5 +2,7 @@ call "%~dp0Cleanup.cmd"
 
 robocopy "%~dp0..\DeveLineStateSaver\bin\Release\netstandard2.0" "%~dp0Output" /E /xf *.pdb
 
-"%~dp0\7z_x64_1805\7za.exe" a -mm=Deflate -mfb=258 -mpass=15 "%~dp0DeveLineStateSaver.zip" "%~dp0Output\*"
-"%~dp0\7z_x64_1805\7za.exe" a -t7z -m0=LZMA2 -mmt=on -mx9 -md=1536m -mfb=273 -ms=on -mqs=on -sccUTF-8 "%~dp0DeveLineStateSaver.7z" "%~dp0Output\*"
+rem "%~dp0\7z_x64_1805\7za.exe" a -mm=Deflate -mfb=258 -mpass=15 "%~dp0DeveLineStateSaver.zip" "%~dp0Output\*"
+rem "%~dp0\7z_x64_1805\7za.exe" a -t7z -m0=LZMA2 -mmt=on -mx9 -md=1536m -mfb=273 -ms=on -mqs=on -sccUTF-8 "%~dp0DeveLineStateSaver.7z" "%~dp0Output\*"
+7zip_x64.exe a -mm=Deflate -mfb=258 -mpass=15 "%~dp0DeveLineStateSaver.zip" "%~dp0Output\*"
+7zip_x64.exe a -t7z -m0=LZMA2 -mmt=on -mx9 -md=1536m -mfb=273 -ms=on -mqs=on -sccUTF-8 "%~dp0DeveLineStateSaver.7z" "%~dp0Output\*"
